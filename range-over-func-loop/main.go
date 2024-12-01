9d3553567f12faefbc933629aa8dd52a5bc3249e
nanikasi <nanikasi1024@gmail.com>
2024-12-01 12:56:11 +0000

    add range over func demo

---
+++ range-over-func-loop/main.go
@@ -0,0 +1,46 @@
+package range_over_func_loop
+
+import (
+	"fmt"
+	"iter"
+)
+
+func main() {
+    // Set型: 重複しない要素の集合を表すコンテナ型
+	mySet := NewSet()
+
+    // 要素を追加
+	mySet.Add(1)
+	mySet.Add(2)
+	mySet.Add(3)
+
+    // mySetの要素を2倍して出力するループ処理
+	for value := range mySet.All() {
+		fmt.Println(value * 2)
+	}
+}
+
+// イテレータ関数を返すメソッドの実装
+func (s *Set) All() iter.Seq[int] {
+    return func(yield func(int) bool) {
+        for v := range s.elements {
+            if !yield(v) {
+                return
+            }
+        }
+    }
+}
+
+type Set struct {
+    elements map[int]struct{}
+}
+
+func NewSet() *Set {
+    return &Set{
+        elements: make(map[int]struct{}),
+    }
+}
+
+func (s *Set) Add(value int) {
+    s.elements[value] = struct{}{}
+}
